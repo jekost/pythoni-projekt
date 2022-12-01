2af65fd12655ed315c004d3f595c1e4b4b3153ca
Joonas Meister <joonas838@gmail.com>
2022-12-01 13:27:02 +0200

    Lisasid vajalike funktsioone

--- luger.py
+++ luger.py
@@ -1,5 +1,7 @@
 
-def faili_lugemine(f):
+# loeb info listidesse 
+def faili_lugemine():
+    f = open('info.txt', encoding = 'utf-8')
 
     while True :
         jook.append(f.readline().strip().split('.  ')[1:])
@@ -10,13 +12,13 @@ def faili_lugemine(f):
         pilt.append(f.readline().strip().strip()[:])
         
         if f.readline() == '':
+            f.close()
             break
     
+    
 
-
+# lisab sõnastikku joogi nime järgi retsepti, kirjelduse ja pildi
 def retsepti(jook, retsept, kirjeldus, pilt):
-    a = ()
-    b = ()
     for i in range(len(jook)):
         retseptid[jook[i][0]] = [(retsept[i])]
         
@@ -40,25 +42,103 @@ def sisendi_otsimine(sisend):#listina
     for jook in sobilikud_joogid:
         if len(sisend) == valitud_joogid.count(jook):
             lõplikud_joogid.append(jook)
-    
-    for jook in lõplikud_joogid:
-        print(jook)
-        print(retseptid[jook][0])
-        print(retseptid[jook][1])
+
+    return lõplikud_joogid
+
+# valitud joogi väljastmaiseks  
+def joogiväljund(jook):
+    print(jook)
+    print(retseptid[jook][0])
+    print(retseptid[jook][1])
+
+# otsib olulisemad joogid listidest ja tagastab nende listi
+def koostis_osadenimi(retsept):
+    ainult_vajalikud_koostisosad = []
+    for i in range(len(retsept)):
+        sisu_eraldi = retsept[i].split(',')
+        
+        for element in sisu_eraldi:
+            if 'ml' in element:
+                vajalik = element.split('ml ')
+                if vajalik[1] not in ainult_vajalikud_koostisosad:
+                    ainult_vajalikud_koostisosad.append(vajalik[1])
+            
+            if 'dashes of' in element:
+                vajalik = element.split('dashes of ')
+                if vajalik[1] not in ainult_vajalikud_koostisosad:
+                    ainult_vajalikud_koostisosad.append(vajalik[1])
+                else:
+                    continue
+            elif 'dashes'  in element:
+                vajalik = element.split('dashes ')
+                if vajalik[1] not in ainult_vajalikud_koostisosad:
+                    ainult_vajalikud_koostisosad.append(vajalik[1])
                 
+            elif 'dash' in element:
+                vajalik = element.split('dash ')
+                if vajalik[1] not in ainult_vajalikud_koostisosad:
+                    ainult_vajalikud_koostisosad.append(vajalik[1])
+                     
+    return ainult_vajalikud_koostisosad
 
-f = open('info.txt', encoding = 'utf-8')
+def retseptid_otsinguks(retseptid, vajalikud_koostisosad):
+    retseptid_otsing ={} 
+    for võti, sisu in retseptid.items():
+        list_elementidest = sisu_eraldi(sisu[0])
+        retseptid_otsing[võti] = list_elementidest
+    return retseptid_otsing
+
+def sisu_eraldi(string):
+    ainult_vajalikud_koostisosad = []
+    järjend = string.split(',')
+    for element in järjend:
+        if 'ml' in element:
+            vajalik = element.split('ml ')
+            if vajalik[1] not in ainult_vajalikud_koostisosad:
+                ainult_vajalikud_koostisosad.append(vajalik[1])
+        
+        if 'dashes of' in element:
+            vajalik = element.split('dashes of ')
+            if vajalik[1] not in ainult_vajalikud_koostisosad:
+                ainult_vajalikud_koostisosad.append(vajalik[1])
+            else:
+                continue
+        elif 'dashes'  in element:
+            vajalik = element.split('dashes ')
+            if vajalik[1] not in ainult_vajalikud_koostisosad:
+                ainult_vajalikud_koostisosad.append(vajalik[1])
+            
+        elif 'dash' in element:
+            vajalik = element.split('dash ')
+            if vajalik[1] not in ainult_vajalikud_koostisosad:
+                ainult_vajalikud_koostisosad.append(vajalik[1])
+    return ainult_vajalikud_koostisosad
+
+
+              
+
+# kogu sõnastik kus on kõik joogi kohta käiv info
 retseptid = {}
-jook = []
-retsept = []
-kirjeldus = []
-pilt = []
+jook = [] #Jookide nimed
+retsept = [] # jookide retseptid listis
+kirjeldus = [] # jookide kirjeldus listis
+pilt = [] # jookide pildi url listis
 
-
-faili_lugemine(f)
-
+# Loeb sisu failist
+faili_lugemine()
+#faili sisu teeb sõnastikuks
 retsepti(jook, retsept, kirjeldus, pilt)
 
-sisendi_otsimine(['Rum', 'Mint'])
+#joogid mis lähevad valiku võimalusse
+vajalikud_koostisosad = koostis_osadenimi(retsept)
 
-f.close()
+# siin on sõnastik kus on joogid ja nende otsinguks vaja minevad asjad
+retseptid_otsing = retseptid_otsinguks(retseptid, vajalikud_koostisosad)
+
+
+#print(joogiväljund('Mojito'))
+
+
+print(retseptid_otsing)
+
+#print(sisendi_otsimine(['Rum', 'Lime Juice']))
