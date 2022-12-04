11abb675624f78a3b411a6d9af1da84c03b63c26
Jan Erik Köst <kost@ut.ee>
2022-12-04 22:57:32 +0200

    saad kätte retsepti,pildi,koostiosad(küll läbi runi)

--- tkinter1.py
+++ tkinter1.py
@@ -1,5 +1,4 @@
 from luger import *
-
 from tkinter import *
 from tkinter import ttk
 
@@ -14,12 +13,43 @@ def tagasta():
             except:
                 output.append(koostisosad[i])
         i+=1
-    print(output) #printib valitud koostisosad
-    print(sisendi_otsimine(output))
-    print()
+
+
+
+
+
+    for widget in vaheraam.winfo_children():
+        widget.destroy()
+
+    misJooki = ttk.Label(vaheraam, text="Mis jooki sa teha tahad", background="lavender")
+    misJooki.grid(row=0,sticky="n")
+
+    rida=1
+    lb=Listbox(vaheraam)
+
+    for jook in sisendi_otsimine(output):
+        lb.insert(rida,jook)
+        rida+=1
+
+    lb.grid()
+
+    lb.bind("<<ListboxSelect>>", lbValik)
+
+def lbValik(synd):
+    valik = synd.widget.curselection()
+    if valik:
+        index = valik[0]
+        data = synd.widget.get(index)
+        print()
+        for el in joogiväljund(data):
+            print (el)
+
+    else:
+        print("")
+
 
 def tee_kastid():
-    rida = 1
+    rida = 2
     global kastid
     kastid = {}
     for koostisosa in range(len(koostisosad)):
@@ -32,9 +62,10 @@ def tee_kastid():
         current_box.var = current_var
         kastid[current_box] = nimi
 
+
 def tee_kastid_otsi():#teeb ainult need kastid kus on sõne sees ("u"->"Rum","Sugar")
     sõne=tk_name.get()
-    rida = 1
+    rida = 2
     global kastid
     global vähem_koostisosi
     kastid = {}
@@ -54,13 +85,13 @@ def tee_kastid_otsi():#teeb ainult need kastid kus on sõne sees ("u"->"Rum","Su
         kastid[current_box] = nimi
 
 def text_changed(*args):
-    print( tk_name.get() )
+    #print( tk_name.get() )
     for kast in kastid:
         kast.destroy()
     tee_kastid_otsi()
 
 koostisosad=['White Rum', 'Sugar Syrup', 'Lime Juice', 'Gin', 'Dry Vermouth', 'Bourbon', 'Angostura Bitters', 'Orange Bitters', 'Citrus Vodka', 'Triple Sec', 'Cranberry Juice', 'Cachaca', 'Vodka', 'Coffee Liqueur', 'Cream', 'Milk', 'Pineapple Juice', 'Coconut Cream', 'Sweet Red Vermouth', 'Campari', 'Créme de Mure', 'Lemon Juice', 'Tequila', 'Simple Syrup', 'Dark Rum', 'Anejo Rum', 'Cola', 'Rye Whiskey', 'Absinthe', ' Peychauds Bitters', 'Peppered Vodka', 'Sweet Vermouth', 'Tomato Juice', 'Tobasco sauce', 'Salt', 'Pepper', 'Gomme syrup', 'Light Rum', 'Orgeat Syrup', 'Amaretto', 'Egg-White', 'Cherry Heering', 'Benedictine', 'Soda Water', 'Chambord', ' Pineapple Juice', 'Vanilla Vodka', 'Kahlua', 'Bacardi Rum', 'Strawberry Liqueur', 'Lime juice', 'Caorunn Gin', 'Raspberry Syrup', 'Knob Creek Bourbon', 'Bourbon Whiskey', 'Apple Schapps', 'Brandy', 'Créme de Cacao', 'Pink Grapefruit Juice', 'Runny Honey', 'Scotch Whisky', 'Whiskey Liqueur', 'Pisco', 'Orange Juice', 'Champagne', 'Aged Rum', 'Creme de Banane', 'Creme de Mure', 'Orange Liqueur', 'Peach Schnapps', 'Créme de Cassis', 'Prosecco', 'Peach puree', 'Peach Bitters', 'Dry Sherry', 'Grapefruit Juice', 'Irish Cream Liqueur', 'freshly squeezed Orange Juice', 'Cognac', 'Double Cream', 'Raspberry Liqueur', 'Egg White', 'Scotch Whiskey', 'Maraschino Liqueur', 'Vanilla Sugar Syrup', 'Premium Gin', 'Lime Cordial', 'Baileys Cream Liqueur', 'Citron Vodka', 'Elderflower Cordial', 'Passoa', ' Lemon Juice', 'Grapefruit Soda', 'Squeezed Lemon Juice', 'Golden Rum', '151 Rum', 'Falernum', 'of Soda Water', 'Raspberry Vodka', 'Black Raspberry Liqueur', ' Cranberry Juice', 'Yellow Chartreuse', 'Blue WKD', 'Port', ' Black Raspberry Liqueur', 'Melon Liqueur', 'Sweet and Sour mix', 'Lemon and Lime Soda', 'Drambuie', 'Grapefruit Bitters']
-koostisosad=['Tequila', 'White Rum', 'Sugar Syrup', 'Lime Juice', 'Dry Vermouth', 'Gin', 'Sugar']
+#koostisosad=['Tequila', 'White Rum', 'Sugar Syrup', 'Lime Juice', 'Dry Vermouth', 'Gin', 'Sugar']
 
 #MAIN
 main=Tk()
@@ -69,22 +100,28 @@ main.title("cocktail time")
 
 #RAAM KUS SEES ON CHECKBOXID,LABEL
 raam=Canvas(main,bg="lavender")
-raam.grid(sticky="n,e,w,s")
+raam.grid()
 
 #vaheraam
-vaheraam=Frame(raam)
+vaheraam=Frame(raam,bg="lavender")
+vaheraam.grid(column=1,rowspan=20, sticky="e")
 
 #LABEL "Mis koostisosad sul olemas on?"
 silt=ttk.Label(raam,text="Mis koostisosad sul olemas on?",background="lavender")
-silt.grid(sticky="w")
+silt.grid(row=0, sticky="w")
 
 #SEARCHBOX
 tk_name = StringVar()
 tk_name.trace("w",callback=text_changed)
 sisend = Entry(raam, textvariable=tk_name)
-sisend.grid()
+sisend.grid(row=1)
 
 
+misJooki = ttk.Label(vaheraam, text="Mis jooki sa teha tahad", background="lavender")
+misJooki.grid(row=0,sticky="n")
+
+lb=Listbox(vaheraam)
+lb.grid()
 
 #SCROLLWHEEL
 #scroll=Scrollbar(raam, command=raam.yview)
