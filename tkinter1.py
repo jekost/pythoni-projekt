5305f9f98a1597e56e15cb09d62b023437fd9ff9
Jan Erik Köst <kost@ut.ee>
2022-12-13 18:43:12 +0200

    improvement of the search system

--- tkinter1.py
+++ tkinter1.py
@@ -1,21 +1,33 @@
 from luger import *
-import urllib.request
 from tkinter import *
 from PIL import ImageTk, Image, ImageOps
-import io
+import os
 
 
-#FUNK MIS KÄIVATAKSE JUHUL KUI KASTI VAJUTATAKSE
+
+#FUNK MIS KÄIVATAKSE JUHUL KUI KASTI KIRJUTATAKSE MIDAGI
+
 def tagasta():
     i=0
-    output=[]
+    global output
+    output=[]       #need kastid mis on hetkel valitud JA on ka ekraanil
+    global aktiivsed_kastid #nimede järgi
+
     for kast in kastid:
+        if kast.cget("text") in aktiivsed_kastid:
+            if kast.var.get()==0:
+                aktiivsed_kastid.remove(kast.cget("text"))
+
         if kast.var.get()==1:
             try:
                 output.append(vähem_koostisosi[i])
             except:
                 output.append(koostisosad[i])
         i+=1
+
+    aktiivsed_kastid+=output
+    aktiivsed_kastid=list(set(aktiivsed_kastid))
+
 
     for widget in vaheraam.winfo_children():
         widget.destroy()
@@ -26,16 +38,15 @@ def tagasta():
     rida=1
     lb=Listbox(vaheraam,height=20)
 
-    for jook in sisendi_otsimine(output):
+    for jook in sisendi_otsimine(aktiivsed_kastid):
         lb.insert(rida,jook)
         rida+=1
 
     lb.grid()
-
     lb.bind("<<ListboxSelect>>", lbValik)
 
 
-pildid = []
+
 def lbValik(synd):
     valik = synd.widget.curselection()
     if valik:
@@ -71,10 +82,10 @@ def lbValik(synd):
         Label(retseptiraam,text=joogiväljund(data)[1],wraplength=200,justify="left").grid(column=3,row=3,sticky="w")
 
         #loe välja pilt formaadis, et see sobiks tkinterisse
-        data = urllib.request.urlopen(joogiväljund(data)[2]).read()
-        piltAlgne = Image.open(io.BytesIO(data))
-        piltAlgne = ImageOps.contain(piltAlgne, (190, 6969))
-        pilt = ImageTk.PhotoImage(piltAlgne)
+        pildi_link=abs_file_path+joogiväljund(data)[2].split("/",8)[-1]
+        pilt=Image.open(pildi_link)
+        pilt.thumbnail((190,69420), Image.ANTIALIAS)
+        pilt = ImageTk.PhotoImage(pilt)
 
         #pane pilt tkinteri aknasse
         Label(retseptiraam,text="",bg="lavender").grid(column=3,row=4)
@@ -93,28 +104,51 @@ def lbValik(synd):
 def tee_kastid_otsi():#teeb ainult need kastid kus on sõne sees ("u"->"Rum","Sugar")
     sõne=tk_name.get()
     rida = 2
-    tulp=0
     global kastid
     global vähem_koostisosi
     kastid = {}
     vähem_koostisosi=[]
+
     for koostisosa in koostisosad:
         if sõne.lower() in koostisosa.lower():
             vähem_koostisosi.append(koostisosa)
-            #nüüd on olemas väiksem list, {koostisosa:index, ko2:in2, ..}
+
+    global kastid_ekraanil
+    kastid_ekraanil=[]
 
     for koostisosa in range(len(vähem_koostisosi)):
         nimi = vähem_koostisosi[koostisosa]
+        kastid_ekraanil.append(nimi)
         rida += 1
         current_var = IntVar()
+        try:
+            if nimi in aktiivsed_kastid:
+                current_var=IntVar(value=1)
+        except:
+            True
         current_box = Checkbutton(raam, text=nimi, variable=current_var, background="lavender", command=tagasta)
-        current_box.grid(row=rida,column=tulp, sticky=W)
+        current_box.grid(row=rida,column=0, sticky=W)
         current_box.var = current_var
         kastid[current_box] = nimi
-        if not tulp==1:
-            if rida==24:
-                tulp+=1
-                rida=0
+
+    try:
+        for widget in vaheraam.winfo_children():
+            widget.destroy()
+
+        misJooki = Label(vaheraam, text="what are you feeling?", background="lavender")
+        misJooki.grid(row=0, sticky="n")
+
+        rida = 1
+        lb = Listbox(vaheraam, height=20)
+
+        for jook in sisendi_otsimine(aktiivsed_kastid):
+            lb.insert(rida, jook)
+            rida += 1
+
+        lb.grid()
+        lb.bind("<<ListboxSelect>>", lbValik)
+    except:
+        True
 
 
 #iga kord kui tekstikasti midagi kirjutatakse, kustutatakse kõik checkboxid ära
@@ -124,6 +158,15 @@ def text_changed(*args):
         kast.destroy()
     tee_kastid_otsi()
 
+aktiivsed_kastid=[]         #need kastid mis on hetkel valitud, aga ei pruugi olla kuvatud
+pildid = [] #kast kus hoida pilte, millegipärast on see tarvis, muidu miski ei tööta...
+
+
+#pane paika tree et saada rootist piltide kausta
+abs_path = os.path.dirname(__file__)
+rel_path = "/pildid/"
+abs_file_path = abs_path+rel_path
+
 koostisosad=['White Rum', 'Sugar Syrup', 'Lime Juice', 'Gin', 'Dry Vermouth', 'Bourbon', 'Angostura Bitters', 'Orange Bitters', 'Citrus Vodka', 'Triple Sec', 'Cranberry Juice', 'Cachaca', 'Vodka', 'Coffee Liqueur', 'Cream', 'Milk', 'Pineapple Juice', 'Coconut Cream', 'Sweet Red Vermouth', 'Campari', 'Créme de Mure', 'Lemon Juice', 'Tequila', 'Simple Syrup', 'Dark Rum', 'Anejo Rum', 'Cola', 'Rye Whiskey', 'Absinthe', ' Peychauds Bitters', 'Peppered Vodka', 'Sweet Vermouth', 'Tomato Juice', 'Tobasco sauce', 'Salt', 'Pepper', 'Gomme syrup', 'Light Rum', 'Orgeat Syrup', 'Amaretto', 'Egg-White', 'Cherry Heering', 'Benedictine', 'Soda Water', 'Chambord', ' Pineapple Juice', 'Vanilla Vodka', 'Kahlua', 'Bacardi Rum', 'Strawberry Liqueur', 'Lime juice', 'Caorunn Gin', 'Raspberry Syrup', 'Knob Creek Bourbon', 'Bourbon Whiskey', 'Apple Schapps', 'Brandy', 'Créme de Cacao', 'Pink Grapefruit Juice', 'Runny Honey', 'Scotch Whisky', 'Whiskey Liqueur', 'Pisco', 'Orange Juice', 'Champagne', 'Aged Rum', 'Creme de Banane', 'Creme de Mure', 'Orange Liqueur', 'Peach Schnapps', 'Créme de Cassis', 'Prosecco', 'Peach puree', 'Peach Bitters', 'Dry Sherry', 'Grapefruit Juice', 'Irish Cream Liqueur', 'freshly squeezed Orange Juice', 'Cognac', 'Double Cream', 'Raspberry Liqueur', 'Egg White', 'Scotch Whiskey', 'Maraschino Liqueur', 'Vanilla Sugar Syrup', 'Premium Gin', 'Lime Cordial', 'Baileys Cream Liqueur', 'Citron Vodka', 'Elderflower Cordial', 'Passoa', ' Lemon Juice', 'Grapefruit Soda', 'Squeezed Lemon Juice', 'Golden Rum', '151 Rum', 'Falernum', 'of Soda Water', 'Raspberry Vodka', 'Black Raspberry Liqueur', ' Cranberry Juice', 'Yellow Chartreuse', 'Blue WKD', 'Port', ' Black Raspberry Liqueur', 'Melon Liqueur', 'Sweet and Sour mix', 'Lemon and Lime Soda', 'Drambuie', 'Grapefruit Bitters']
 #koostisosad=['Tequila', 'White Rum', 'Sugar Syrup', 'Lime Juice', 'Dry Vermouth', 'Gin', 'Sugar']
 
@@ -131,7 +174,6 @@ koostisosad=['White Rum', 'Sugar Syrup', 'Lime Juice', 'Gin', 'Dry Vermouth', 'B
 main=Tk()
 main.title("cocktail time")
 main.resizable(False, False)
-#main.geometry("600x240")
 
 #RAAM KUS SEES ON CHECKBOXID,LABEL
 raam=Canvas(main,bg="lavender")
@@ -157,11 +199,7 @@ joogiretsept=Label(retseptiraam,text="the recipe for your drink:",bg="lavender")
 joogiretsept.grid(column=3,row=2,sticky="nw")
 
 
-
-
-
-
-#LABEL "Mis koostisosad sul olemas on?"
+#LABEL "what ingredients do you currently have?"
 silt=Label(raam,text="what ingredients do you currently have?",background="lavender")
 silt.grid(row=0, sticky="w")
 
@@ -171,16 +209,12 @@ tk_name.trace("w",callback=text_changed)
 sisend = Entry(raam, textvariable=tk_name)
 sisend.grid(row=1)
 
-
+#LABEL "what are you feeling?"
 misJooki = Label(vaheraam, text="what are you feeling?", background="lavender")
 misJooki.grid(row=0,sticky="n")
 
 lb=Listbox(vaheraam,height=20)
 lb.grid()
-
-
-#print(anna_joogid())
-
 
 
 
